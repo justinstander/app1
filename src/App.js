4066005bcfcd431abaac2d4653fcf4028ffa0805
justinstander <justin@sundaygoods.com>
2019-12-03 15:03:17 -0500

    Added Bootstrap. Changed placeholder.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Jumbotron className="Jumbotron">
+          <div className="Jumbotron-text">
+            <h1>Coming Soon</h1>
+            <h2>The Greatest Advertising Agency Ever</h2>
+          </div>
+        </Jumbotron>
       </header>
     </div>
   );
