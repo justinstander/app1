f414d06866849c16ed6c256d5c439047b81bc405
justinstander <justin@sundaygoods.com>
2019-12-18 21:32:15 -0500

    Additional Lint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,5 @@
 export const GET_TOTAL_COST = "GET_TOTAL_COST";
 
 export const getTotalCost = () => {
- 	return {
-    	type: GET_TOTAL_COST,
-    	totalCost: "$0.50"
-  	};
+	return { type: GET_TOTAL_COST, totalCost: "$0.50" };
 };
--- src/reducers/main.js
+++ src/reducers/main.js
@@ -3,8 +3,8 @@ import { GET_TOTAL_COST } from "../actions";
 export default (state = {}, action) => {
 	switch(action.type) {
 		case GET_TOTAL_COST:
-			return { ...state, totalCost: action.totalCost};
+			return {...state, totalCost: action.totalCost};
 		default:
 			return state;
 	}
-}
+};
