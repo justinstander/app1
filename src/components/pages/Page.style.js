87294a6c9f1182e1bb1e7f57a2269cac0d158958
justinstander <justin@sundaygoods.com>
2020-01-03 12:23:32 -0500

    Max height for a page since there is padding on the top now.

--- src/components/pages/Page.style.js
+++ src/components/pages/Page.style.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 
 export const PageContainer = styled(Container)`
   height: 100%;
+  max-height: 100%;
   width: 100%;
   padding-top: 3.5rem;
   display: flex;
